a9375fdd75c07ac8332d6ab2f1ecfefaf25ecde9
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-03 21:32:27 +0000

    Use The abs builtin

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+
 if (number < 0):
-    last_digit = ((number * -1) % 10) * -1
-else:
-    last_digit = (number) % 10
+    last_digit *= -1
+
 if (last_digit > 5):
     digit_type = "and is greater than 5"
 elif (last_digit == 0):
