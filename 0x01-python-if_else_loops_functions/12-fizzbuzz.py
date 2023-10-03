ded2a748f9bc41b0eefa00c7520733fc8025513c
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-03 23:44:51 +0000

    Check for multiples of 15 first

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@
 def fizzbuzz():
     """Play the Fizz Buzz game."""
     for num in range(1, 101):
-        if (num % 3 == 0):
-            print("Fizz", end=" ")
+        if (num % 15 == 0):
+            print("FizzBuzz", end=" ")
         elif (num % 5 == 0):
             print("Buzz", end=" ")
         elif (num % 15 == 0):
-            print("FizzBuzz", end=" ")
+            print("Fizz", end=" ")
         else:
             print(num, end=" ")
