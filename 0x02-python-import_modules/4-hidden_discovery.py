8ca18327e354aa26921762382f5960a942428bdf
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-06 00:07:32 +0000

    Remove the imported file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "main":
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    pass
+
+import hidden_4
+
+names = dir(hidden_4)
+for name in names:
+    if name[:2] != "__":
+        print(name)
