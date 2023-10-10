ea69991634671673e94fac5b296b3cf6c3cad0d0
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-10 01:11:10 +0000

    Solve the comparison error between int and None types

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 def max_integer(my_list=[]):
     """Write a function that finds the biggest integer of a list."""
 
-    if my_list is None:
+    if my_list is None or my_list == []:
         return None
-    max_num = None
+    max_num = 0
     for num in my_list:
         if num > max_num:
             max_num = num
