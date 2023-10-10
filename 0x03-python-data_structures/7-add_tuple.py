6bfe903bbbefff9999c64b3a2a8a43d7e0255963
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-10 00:51:38 +0000

    Fix the pycodestyle error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a,b = 0,0
+    a, b = 0, 0
     if len(tuple_a) >= 1:
         a += tuple_a[0]
     if len(tuple_b) >= 1:
@@ -11,5 +11,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b += tuple_b[1]
     new_tuple = (a, b)
     return (new_tuple)
-
-        
