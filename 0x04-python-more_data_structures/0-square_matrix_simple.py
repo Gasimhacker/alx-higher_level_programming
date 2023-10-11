48c2301da28c7635488ca40b97ae7b1d1a1789d6
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-11 21:21:30 +0000

    Use a simpler method

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,5 @@
 def square_matrix_simple(matrix=[]):
     """Compute the square value of all integers of a matrix."""
 
-    sqr_matrix = []
-    for a_list in matrix:
-        sqr_matrix.append([x ** 2 for x in a_list])
+    sqr_matrix = [[x ** 2 for x in a_list] for a_list in matrix]
     return sqr_matrix
