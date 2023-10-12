2c0c4708d012b6e646903bcf006ea15277433db7
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-12 00:12:59 +0000

    Loop through the dict for one time

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,6 @@ def best_score(a_dictionary):
 
     if a_dictionary is None:
         return None
-    a = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
-    return a[0][0]
+    a = dict(sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
+    for k in a:
+        return k
