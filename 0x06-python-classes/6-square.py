309fd44f6fc3d9ac9becf581f315a4f2051c9619
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-25 01:19:57 +0000

    Check if the numbers are positive integers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,10 @@ class Square:
     def size(self, value):
         """Change the position"""
 
-        if not isinstance(value, tuple) or len(value) != 2:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
