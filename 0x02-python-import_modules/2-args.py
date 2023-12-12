a227e995732b310a3cb0458e498a6e5b97697842
Mohammed Ahmed <ma2286572@gmail.com>
2023-12-12 17:46:51 +0000

    Disable execution when importing for the rest of the files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    pass
+    from sys import argv
 
-from sys import argv
+    argstr = "arguments"
+    argc = len(argv) - 1
+    mark = ":"
 
-argstr = "arguments"
-argc = len(argv) - 1
-mark = ":"
+    if (argc == 1):
+        argstr = "argument"
+    if (argc == 0):
+        mark = "."
 
-if (argc == 1):
-    argstr = "argument"
-if (argc == 0):
-    mark = "."
+    print("{} {}{}".format(argc, argstr, mark))
 
-print("{} {}{}".format(argc, argstr, mark))
-
-for arg in range(1, argc + 1):
-    print("{}: {}".format(arg, argv[arg]))
+    for arg in range(1, argc + 1):
+        print("{}: {}".format(arg, argv[arg]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    pass
+    from sys import argv
 
-from sys import argv
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
 
-sum = 0
-for i in range(1, len(argv)):
-    sum += int(argv[i])
-
-print(sum)
+    print(sum)
