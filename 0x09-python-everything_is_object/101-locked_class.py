f9ead36134bd768a3ea87a23d6cc41fa1fd42449
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-06 02:10:27 +0000

    Delete the trailing whitespace and add blank lines

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
-""" 
+"""
 
 This module contains a class called locked class
 that prevent creating a new instance attribute accidentally
 when trying to access the attributes defined in the class
 
 """
+
+
 class LockedClass:
-    """ 
+    """
 
     This class will prevent the user from dynamically
     creating new instance attributes, except if
