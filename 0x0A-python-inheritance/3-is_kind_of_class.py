6a90831207f94f545f3c32bf0b9b00ec92cd5b78
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-09 00:09:27 +0000

    Edit the long line and use objects instead of thier classes

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -19,8 +19,8 @@ def is_kind_of_class(obj, a_class):
 
     Returns:
         True if object is an instance of the specified class or if the
-        object is an instance of a class that inherited from, the specified class
-        otherwise False.
+        object is an instance of a class that inherited from,
+        the specified class; otherwise False.
     """
 
-    return (isinstance(type(obj), a_class))
+    return (isinstance(obj, a_class))
