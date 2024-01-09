311d0fb600f6c77a8f59c0c042bb821b822444bd
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-09 00:16:40 +0000

    Check if the an object's class is a subclass of a specific class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""
+
+This module contains a function that tells if the
+object is an instance of a class
+that inherits from a specific class(directly or indirectly)
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a class
+    that inherits from a specific class(directly or indirectly)
+
+    Args:
+        obj (object): Any type of object can be passed
+        a_class: Any class can be passed
+
+    Returns:
+        True if object is an instance of a class that
+        inherited from the specific class; otherwise False.
+    """
+
+    return (issubclass(type(obj), a_class))
