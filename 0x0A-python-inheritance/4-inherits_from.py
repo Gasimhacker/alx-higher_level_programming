9cde666349d0b85a53ff78134cc21bb6eb5c2d3a
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-09 00:21:10 +0000

    Assure that the object isn't an instance of the specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -24,4 +24,6 @@ def inherits_from(obj, a_class):
         inherited from the specific class; otherwise False.
     """
 
-    return (issubclass(type(obj), a_class))
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
