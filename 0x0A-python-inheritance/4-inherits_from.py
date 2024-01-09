d1e82d0e012e5223a28512b853400c4c181f14c9
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-09 00:23:51 +0000

    Write an empty class BaseGeometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -24,6 +24,6 @@ def inherits_from(obj, a_class):
         inherited from the specific class; otherwise False.
     """
 
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) is a_class:
         return True
     return False
