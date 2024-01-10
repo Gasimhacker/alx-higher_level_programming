a2fde57751e980e10d228dd4461e0745a19c8571
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-10 06:37:15 +0000

    Redo the student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
-"""
+
+"""This module contains a class called Student"""
 
 
 class Student:
-    """ Class to create student instances """
+    """A student class"""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Inintialze a student object
+
+        Args:
+            first_name (str): The student's first name
+            last_name (str): The student's last name
+            age (int): The student's age
+
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+        """Create a JSON representation for Student objects"""
+
+        json = {"first_name": self.first_name, "last_name": self.last_name,
+                "age": self.age}
+        return json
