7fcaa84345b940f32fed7c9e58d5cc85b420f695
Mohammed Ahmed <ma2286572@gmail.com>
2024-01-15 23:25:58 +0000

    Revert "Try these tests"
    This reverts commit a4dc40b4380b97dd2856875e1b39c8d775d33dd4.
    
    Revert "Try these tests"
    
    This reverts commit a4dc40b4380b97dd2856875e1b39c8d775d33dd4.

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -118,47 +118,49 @@ class TestBase_init(unittest.TestCase):
 class TestBase_to_json(unittest.TestCase):
     """Unittest to test the method to_json_string of the Base class"""
 
-    def test_to_json_string_rectangle_type(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
+    def test_to_json_rectangle_type(self):
+        d = Rectangle(3, 5, 7, 9, 10).to_dictionary()
+        self.assertEqual(str, type(Base.to_json_string([d])))
 
-    def test_to_json_string_rectangle_one_dict(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertTrue(len(Base.to_json_string([r.to_dictionary()])) == 53)
+    def test_to_json_rectangle_one_dict(self):
+        d = Rectangle(3, 5, 7, 9, 10).to_dictionary()
+        self.assertEqual([d], eval((Base.to_json_string([d]))))
+        self.assertTrue(len(Base.to_json_string([d])) == 53)
 
-    def test_to_json_string_rectangle_two_dicts(self):
-        r1 = Rectangle(2, 3, 5, 19, 2)
-        r2 = Rectangle(4, 2, 4, 1, 12)
-        list_dicts = [r1.to_dictionary(), r2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts)) == 106)
+    def test_to_json_rectangle_two_dict(self):
+        d1 = Rectangle(3, 5, 7, 9, 10).to_dictionary()
+        d2 = Rectangle(2, 4, 6, 8, 11).to_dictionary()
+        self.assertEqual([d1, d2], eval((Base.to_json_string([d1, d2]))))
+        self.assertTrue(len(Base.to_json_string([d1, d2])) == 106)
 
-    def test_to_json_string_square_type(self):
-        s = Square(10, 2, 3, 4)
-        self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
+    def test_to_json_square_type(self):
+        d = Square(3, 7, 9, 10).to_dictionary()
+        self.assertEqual(str, type(Base.to_json_string([d])))
 
-    def test_to_json_string_square_one_dict(self):
-        s = Square(10, 2, 3, 4)
-        self.assertTrue(len(Base.to_json_string([s.to_dictionary()])) == 39)
+    def test_to_json_square_one_dict(self):
+        d = Square(3, 7, 9, 10).to_dictionary()
+        self.assertEqual([d], eval((Base.to_json_string([d]))))
+        self.assertTrue(len(Base.to_json_string([d])) == 39)
 
-    def test_to_json_string_square_two_dicts(self):
-        s1 = Square(10, 2, 3, 4)
-        s2 = Square(4, 5, 21, 2)
-        list_dicts = [s1.to_dictionary(), s2.to_dictionary()]
-        self.assertTrue(len(Base.to_json_string(list_dicts)) == 78)
+    def test_to_json_square_two_dict(self):
+        d1 = Square(3, 7, 9, 10).to_dictionary()
+        d2 = Square(2, 6, 8, 11).to_dictionary()
+        self.assertEqual([d1, d2], eval((Base.to_json_string([d1, d2]))))
+        self.assertTrue(len(Base.to_json_string([d1, d2])) == 78)
 
-    def test_to_json_string_empty_list(self):
+    def test_to_json_empty_list(self):
         self.assertEqual("[]", Base.to_json_string([]))
 
-    def test_to_json_string_none(self):
+    def test_to_json_None(self):
         self.assertEqual("[]", Base.to_json_string(None))
 
-    def test_to_json_string_no_args(self):
+    def test_to_json_no_arg(self):
         with self.assertRaises(TypeError):
             Base.to_json_string()
 
-    def test_to_json_string_more_than_one_arg(self):
+    def test_to_json_more_than_one_arg(self):
         with self.assertRaises(TypeError):
-            Base.to_json_string([], 1)
+            Base.to_json_string([], 2)
 
 
 class TestBase_save_to_file(unittest.TestCase):
