9026ecbe568d37e9b8d0ebffc318c4aeae242b1c
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-13 23:56:23 +0000

    Count the records with the same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- List the number of records with the same score in the table second_table
+-- The result will display the score and
+-- the number of records for this score with the label number
+SELECT score, count(score) AS 'number'
+from second_table
+GROUP BY score;
