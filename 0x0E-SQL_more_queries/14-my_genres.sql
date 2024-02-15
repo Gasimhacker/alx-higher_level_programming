e46a86b2028184e5f2843d3d4c160c4ea05dddb6
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-15 07:09:47 +0000

    Put the semicolon in the last line

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,7 +5,7 @@ SELECT tv_genres.name
   FROM tv_genres INNER JOIN
 		tv_show_genres
 		ON tv_show_genres.genre_id = tv_genres.id
- WHERE tv_show_genres.show_id = (SELECT id
+ WHERE tv_show_genres.show_id IN (SELECT id
 				    FROM tv_shows
-				   WHERE title = 'Dexter');
- ORDER BY tv_genres.name
+				   WHERE title = 'Dexter')
+ ORDER BY tv_genres.name;
