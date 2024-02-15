1897a6761e5c48f54d64b36243ae27463ea52127
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-15 00:23:26 +0000

    Use tabs for readability

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -3,4 +3,6 @@
 -- name VARCHAR(256)
 -- If the table `id_not_null` already exists, the script should not fail
 CREATE TABLE IF NOT EXISTS id_not_null (
-	id INT DEFAULT 1, name VARCHAR(256));
+	id 	INT 		DEFAULT 1,
+	name 	VARCHAR(256)
+);
