083efcfbf482bcca6bee76ddd6feeb35ea619631
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-15 06:45:22 +0000

    Revert "Solve the remaining tasks"
    This reverts commit 1e9d3cc85a1929a8d05c7c223508ae7193453ed2.

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,11 @@
--- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
--- Records are ordered by ascending genre name.
-SELECT g.`name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-
-       INNER JOIN `tv_shows` AS t
-       ON t.`id` = s.`show_id`
-       WHERE t.`title` = "Dexter"
- ORDER BY g.`name`;
+-- List all genres of the show Dexter
+-- Each record should display: tv_genres.name
+-- Results are sorted in ascending order by the genre name
+SELECT tv_genres.name
+  FROM tv_genres INNER JOIN
+		tv_show_genres
+		ON tv_show_genres.genre_id = tv_genres.id
+ WHERE tv_show_genres.show_id = (SELECT id
+				    FROM tv_shows
+				   WHERE title = 'Dexter');
+ ORDER BY tv_genres.name
