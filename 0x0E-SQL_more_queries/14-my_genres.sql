3b54d44a7e070a23c042738967d46373b8dd2b16
Mohammed Ahmed <ma2286572@gmail.com>
2024-02-15 02:43:51 +0000

    List all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,11 @@
+-- List all genres of the show Dexter
+-- Each record should display: tv_genres.name
+-- Results are sorted in ascending order by the genre name
+SELECT tv_genres.name
+  FROM tv_genres INNER JOIN
+		tv_show_genres
+		ON tv_show_genres.genre_id = tv_genres.id
+ WHERE tv_show_genres.show_id = (SELECT id
+				    FROM tv_shows
+				   WHERE title = 'Dexter');
+ ORDER BY tv_genres.name
